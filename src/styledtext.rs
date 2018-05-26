de449f011db0cae7e5fee72893276bbb2849889b
Matt Peterson <ricochet1k@gmail.com>
2018-05-25 21:21:49 -0400

    Add tests for styledtext

--- src/styledtext.rs
+++ src/styledtext.rs
@@ -24,12 +24,14 @@ fn width_slice(txt: &str, a: usize, b: usize) -> &str {
     let mut end_index = txt.len();
     for (i, c) in txt.char_indices() {
         if !found_start {
-            if width_so_far == a {
+            if width_so_far >= a {
                 start_index = i;
                 found_start = true;
             }
             else if width_so_far > a {
-                panic!("Slice in the middle of a double-width char!");
+                //panic!("Slice in the middle of a double-width char!");
+                start_index = i;
+                found_start = true;
             }
         }
         if found_start {
@@ -52,6 +54,7 @@ fn width_slice(txt: &str, a: usize, b: usize) -> &str {
 }
 
 impl StyledText {
+    /// StyledText must not have a text length of 0. If you try it will panic.
     pub fn new(style: Style, text: String) -> StyledText {
         if text.len() == 0 { panic!("Zero width StyledText"); }
 
@@ -59,6 +62,9 @@ impl StyledText {
         StyledText { style, text: Rc::new(text), width: width as _ }
     }
 
+    /// Slice the string returning a new StyledText with the same style. Slicing is done by
+    /// width, rather than by byte or by char. So the returned slice should have exactly the
+    /// width specified.
     pub fn slice<R: RangeBounds<usize>>(&self, r: R) -> StyledText where String: std::ops::Index<R> {
         let a = match r.start() {
             std::ops::Bound::Included(i) => *i,
@@ -68,9 +74,16 @@ impl StyledText {
         let b = match r.end() {
             std::ops::Bound::Included(i) => *i+1,
             std::ops::Bound::Excluded(i) => *i,
-            std::ops::Bound::Unbounded => self.text.len(),
+            std::ops::Bound::Unbounded => self.width as _,
         };
-        let n = StyledText::new(self.style, width_slice(&self.text, a, b).to_string());
+        //let n = StyledText::new(self.style, width_slice(&self.text, a, b).to_string());
+        let sliced = width_slice(&self.text, a, b).to_string();
+        let n = StyledText {
+            style: self.style,
+            text: Rc::new(sliced),
+            width: (b - a) as u32,
+        };
+            
         //println!("\n\rStyledText::slice: {}..{}  {:?}", a, b, n);
         n
     }
@@ -88,3 +101,46 @@ impl PaintableWidget for StyledText {
     }
 }
 
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::fmt::Debug;
+    use std::clone::Clone;
+
+    #[test]
+    #[should_panic]
+    fn no_empty_styledtext() {
+        StyledText::new(Style::default(), "".to_string());
+    }
+
+    fn slice_test<R: RangeBounds<usize> + Clone + Debug>(s: &str, r: R, sl: &str, w: u32) where String: std::ops::Index<R> {
+        let slice = StyledText::new(Style::default(), s.to_string()).slice(r.clone());
+        assert_eq!(*slice.text, sl,
+                  "Slice str is incorrect: want: {:?}, got {:?}", sl, slice.text);
+        assert_eq!(slice.width, w,
+                  "Slice width is incorrect: {:?}.slice({:?}) width is {}, wanted {}", s, r, slice.width, w);
+    }
+
+    #[test]
+    fn slicing() {
+        slice_test("asdf", .., "asdf", 4);
+        slice_test("asdf", 0..4, "asdf", 4);
+        slice_test("asdf", 1..3, "sd", 2);
+        slice_test("台北1234", .., "台北1234", 8); // 2 double-width chars
+        slice_test("台北1234", 0..2, "台", 2); // 2 double-width chars
+
+        // TODO: This keeps 1 double width char, even though we only asked for 1 width.
+        //       Is this what terminal emulators do?
+        slice_test("台北1234", 0..1, "台", 1);
+
+        // TODO: This tries to start a slice in the middle of one char. What to do?
+        //       We definitely don't want to crash.
+        slice_test("台北1234", 1..2, "", 1);
+
+        slice_test("台北1234", 2..4, "北", 2);
+        slice_test("ＱＲＳ12", .., "ＱＲＳ12", 8);
+        slice_test("ｱｲｳ1234", .., "ｱｲｳ1234", 7); // 3 single-width chars
+    }
+
+}
