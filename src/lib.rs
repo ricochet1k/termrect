af03d519a15c59acc38318dbba990d32e4f9a451
Matt Peterson <ricochet1k@gmail.com>
2018-06-06 10:50:48 -0400

    Termion is optional, move to feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,18 @@
 #[macro_use]
 extern crate bitfield;
 extern crate itertools;
-extern crate termion;
 extern crate unicode_width;
 
 pub mod delta;
 pub mod line;
 pub mod style;
 pub mod styledtext;
-pub mod terminal;
 pub mod termrect;
+
+#[cfg(feature = "termion")]
+extern crate termion;
+#[cfg(feature = "termion")]
+pub mod terminal;
 
 pub use style::{Color, Style, StyleAttr};
 pub use termrect::TermRect;
